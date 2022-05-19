33d4b060bb1fcaf488935e953e1df453241e7e64
Diego M. Dominguez <diegomdomi@gmail.com>
2022-05-19 20:55:07 +0200

    change in GET/movie

--- src/components/ApiMovie.jsx
+++ src/components/ApiMovie.jsx
@@ -11,7 +11,7 @@ const ApiMovie = () => {
             fetchData()
         },[])
 
-const baseUrl = 'https://api.themoviedb.org/3/movie/popular?'
+const baseUrl = 'https://api.themoviedb.org/3/movie/top_rated?'
 const apiKey = process.env.REACT_APP_APIKEY;
 
 function fetchData(){
