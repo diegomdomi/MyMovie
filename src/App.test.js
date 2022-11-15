c585eb6431ed30bdd874e8c4e1d9cd7d39d18b36
diegomdomi <diegomdomi@gmail.com>
2022-11-15 14:12:03 +0100

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen,create } from '@testing-library/react';
 import App from './App';
-
+import ApiMovie from './components/ApiMovie'
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = create(<ApiMovie/>);
   expect(linkElement).toBeInTheDocument();
 });
